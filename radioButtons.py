17654cb1cbba5f5661b07c9c0e3bbf96244fd88e
Soumya Lakshmi <soumyalakshmi@Soumyas-MacBook-Air.local>
2024-01-19 09:52:13 -0500

    adding modified files

--- radioButtons.py
+++ radioButtons.py
@@ -22,20 +22,22 @@ buttonClick("radio1")
 
 driver.find_element(By.XPATH, "//input[@id='autocomplete']").send_keys("ind")
 time.sleep(10)
-
-
-
-
 print(driver.find_element(By.XPATH, "//legend[text()='Suggession Class Example']").text)
 countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item']")
 print(len(countries))
-for country in countries:
-    if country.text == "India":
-        country.click()
-        break
-assert driver.find_element(By.XPATH, "//input[@id='autocomplete']").get_attribute("value") == "India"
 
+
+def auto_sugesion(country_name):
+    for country in countries:
+        if country.text == country_name:
+            country.click()
+            break
+
+
+auto_sugesion("India")
+assert driver.find_element(By.XPATH, "//input[@id='autocomplete']").get_attribute("value") == "India"
 print(driver.find_element(By.XPATH, "//input[@id='autocomplete']").get_attribute("value"))
+
 time.sleep(10)
 
 print(driver.find_element(By.XPATH, "//legend[text()='Dropdown Example']").text)
@@ -46,16 +48,19 @@ time.sleep(10)
 
 print(driver.find_element(By.XPATH, "//legend[text()='Checkbox Example']").text)
 checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
-for checkbox in checkboxes:
-    if checkbox.get_attribute("value") == "option3":
-        checkbox.click()
-        assert checkbox.is_selected()
-        break
+
+
+def check_box(option):
+    for checkbox in checkboxes:
+        if checkbox.get_attribute("value") == option:
+            checkbox.click()
+            assert checkbox.is_selected()
+            break
+
+
 time.sleep(10)
+check_box("option3")
 
 print(driver.find_element(By.XPATH, "//legend[text()='Switch Window Example']").text)
 driver.find_element(By.XPATH, "//button[@id='openwindow']").click()
 time.sleep(10)
-
-
-
