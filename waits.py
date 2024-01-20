3516efb6461a3c7bbff75dd9e273c80bfc7b0fc3
Soumya Lakshmi <soumyalakshmi@Soumyas-MacBook-Air.local>
2024-01-20 07:29:58 -0500

    adding modification

--- waits.py
+++ waits.py
@@ -4,11 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = common.get_driver()
+
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.find_element(By.XPATH, "//input[@type='search']").send_keys("ber")
 time.sleep(10)
+
 products = driver.find_elements(By.XPATH, "//div[@class='product']")
 print(len(products))
 assert (len(products)) > 0
+
 common.product_select(products)
+
 time.sleep(10)
